7e67ecf55998b3171dd54fccc0a4efba2c12fd6f
Akter Hussain <akterdev@gmail.com>
2022-07-18 15:33:58 +0600

    redirect function

--- First Application/index.js
+++ First Application/index.js
@@ -4,6 +4,15 @@ app = express();
 app.get("/download", function(req, res){
     res.download('./assets/akter.jpg');
 })
+
+app.get('/', function(req, res){
+    res.redirect('https://www.gaziakter.com/');
+})
+
+app.get('/about', function(req, res){
+    res.send('This is about page');
+})
+
 app.listen(4040, function(){
     console.log("server run success");
 })
