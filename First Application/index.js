10b8d8d68c166345ea196f2f06ac4010a867b152
Akter Hussain <akterdev@gmail.com>
2022-07-18 16:40:57 +0600

    first name and last name request

--- First Application/index.js
+++ First Application/index.js
@@ -2,11 +2,10 @@ var express = require('express');
 
 app = express();
 
-app.get('/about', function(req, res){
-    res.cookie('Name', 'Akter Hussain');
-    res.cookie('City', 'Dhaka');
-    res.cookie('Age', 35);
-    res.end('cookie set success')
+app.get('/', function(req, res){
+    let firstName = req.query.firstName;
+    let lastName = req.query.lastName;
+    res.end(firstName +' '+lastName);
 })
 
 app.listen(4040, function(){
