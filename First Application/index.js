67704a263fe454c49e2c2bd618190bd2651c6c5b
Akter Hussain <akterdev@gmail.com>
2022-07-23 20:16:47 +0600

    Upload file

--- First Application/index.js
+++ First Application/index.js
@@ -1,16 +1,30 @@
 var express = require('express');
 var multer = require('multer');
-var multer = multer();
 
 var app = express();
-app.use(multer.array());
-app.use(express.static('public'));
+
+var storage = multer.diskStorage({
+    destination:function(req, file, callBack){
+        callBack(null, './uploads');
+    },
+    filename:function(req, file, callBack){
+        callBack(null, file.originalname)
+    }
+})
+
+var upload = multer({storage:storage}).single('myfile');
 
 app.post('/', function(req, res){
-   let jsonData = req.body;
-   res.send(JSON.stringify(jsonData));
-})
+   upload(req, res, function(error){
+        if(error){
+            res.send('file upload fail');
+        } else{
+            res.send('file upload sussess');
+        }
+
+   });
+});
 
 app.listen(4040, function(){
     console.log("server run success");
-})
+});
